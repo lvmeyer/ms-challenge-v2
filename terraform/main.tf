3323f714dc8f623fed71cf1523aaed1812bd0e43
DanLevyM <dan.levy.pro@gmail.com>
2023-06-18 17:36:49 +0200

    terraform - add db

---
+++ terraform/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+  required_version = ">= 1.2.0"
+}
+provider "aws" {
+  region = "eu-west-3"
+}
+
+resource "aws_db_instance" "db_instance" {
+  db_name                = "dbpdt"
+  engine                 = "postgres"
+  engine_version         = "14.6"
+  multi_az               = false
+  identifier             = "pdt-rds-instance"
+  username               = var.db_username
+  password               = var.db_password
+  instance_class         = "db.t3.micro"
+  allocated_storage      = 10
+  db_subnet_group_name   = aws_db_subnet_group.dbpdt-subnet-group.name
+  vpc_security_group_ids = [aws_security_group.db_sg.id]
+  availability_zone      = "eu-west-3a"
+  skip_final_snapshot    = true
+}
