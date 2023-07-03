5a5dd95078ce52dcfa2c29c3dcafd37b1465de50
victor <valee.victor@gmail.com>
2023-07-03 15:11:18 +0200

    dashboard aboard

--- vite-front/src/index.jsx
+++ vite-front/src/index.jsx
@@ -5,6 +5,10 @@ import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import store from './store';
 import { Provider } from 'react-redux';
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 const root = document.getElementById('root');
 
--- vite-front/src/pages/Dashboard/Dashboard.jsx
+++ vite-front/src/pages/Dashboard/Dashboard.jsx
@@ -13,7 +13,25 @@ import React from 'react';
 // import { useMaterialTailwindController, setOpenConfigurator } from "@/context";
 
 export const Dashboard = () => {
-    return <h1>Hello World</h1>;
+    return (
+      <>
+      <div className='vh-100 col-xl-3 col-md-2 d-flex flex-column border pt-2 bg-dark text-white'>
+        <div className='p-3'>
+          <div>LOGO</div>
+        </div>
+        <div className='w-100 p-3'>
+            <div>
+                <div className='py-2'> Dashboard</div>
+                <div className='btn btn-outline-info py-2'>Profile</div>
+            </div>
+        </div>
+        <div className=' p-3'>
+            <div className='py-2'> Users </div>
+            <div className='py-2'> Produits </div>
+        </div>
+      </div>
+      </>
+    );
   };
 
 // Dashboard.displayName = "/src/layout/dashboard.jsx";
