e826de04f403608a52db47354bf3dac26bfccc3e
DanLevyM <dan.levy.pro@gmail.com>
2023-07-04 22:03:03 +0200

    [TEST] sonarqube

---
+++ apis/apps/payment/src/__test__/payment.controller.spec.ts
@@ -0,0 +1,28 @@
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { PaymentController } from '../payment.controller';
+import { PaymentService } from '../payment.service';
+
+describe('PaymentController', () => {
+  let paymentController: PaymentController;
+
+  const mockUserService = {};
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [PaymentController],
+      providers: [PaymentService],
+    })
+      .overrideProvider(PaymentService)
+      .useValue(mockUserService)
+      .compile();
+
+    paymentController = app.get<PaymentController>(PaymentController);
+  });
+
+  describe('root', () => {
+    it('should be defined"', () => {
+      expect(paymentController).toBeDefined();
+    });
+  });
+});
