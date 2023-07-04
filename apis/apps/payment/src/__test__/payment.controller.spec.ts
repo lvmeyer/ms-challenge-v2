5ba1270c4821517a30de907986ecb6afe20b499e
DanLevyM <dan.levy.pro@gmail.com>
2023-07-04 17:39:40 +0200

    k8s - ingress - clusterIP for microservices

--- apis/apps/payment/src/__test__/payment.controller.spec.ts
+++ apis/apps/payment/src/__test__/payment.controller.spec.ts
@@ -1,11 +1,17 @@
-import { Test, TestingModule } from '@nestjs/testing';
+// import { Test, TestingModule } from '@nestjs/testing';
 
+<<<<<<< HEAD:apis/apps/payment/src/__test__/payment.controller.spec.ts
 import { PaymentController } from '../payment.controller';
 import { PaymentService } from '../payment.service';
+=======
+// import { PaymentController } from './payment.controller';
+// import { PaymentService } from './payment.service';
+>>>>>>> 4b6e401 (k8s - ingress - clusterIP for microservices):apis/apps/payment/src/payment.controller.spec.ts
 
-describe('PaymentController', () => {
-  let paymentController: PaymentController;
+// describe('PaymentController', () => {
+//   let paymentController: PaymentController;
 
+<<<<<<< HEAD:apis/apps/payment/src/__test__/payment.controller.spec.ts
   const mockUserService = {};
 
   beforeEach(async () => {
@@ -16,13 +22,31 @@ describe('PaymentController', () => {
       .overrideProvider(PaymentService)
       .useValue(mockUserService)
       .compile();
+=======
+//   beforeEach(async () => {
+//     const app: TestingModule = await Test.createTestingModule({
+//       controllers: [PaymentController],
+//       providers: [PaymentService],
+//     }).compile();
+>>>>>>> 4b6e401 (k8s - ingress - clusterIP for microservices):apis/apps/payment/src/payment.controller.spec.ts
 
-    paymentController = app.get<PaymentController>(PaymentController);
-  });
+//     paymentController = app.get<PaymentController>(PaymentController);
+//   });
 
+<<<<<<< HEAD:apis/apps/payment/src/__test__/payment.controller.spec.ts
   describe('root', () => {
     it('should be defined"', () => {
       expect(paymentController).toBeDefined();
     });
   });
 });
+=======
+//   describe('root', () => {
+//     it('should return "Hello World!"', () => {
+//       expect(paymentController.validPayment()).toEqual({
+//         message: 'Payment is valid',
+//       });
+//     });
+//   });
+// });
+>>>>>>> 4b6e401 (k8s - ingress - clusterIP for microservices):apis/apps/payment/src/payment.controller.spec.ts
--- apis/apps/payment/src/payment.service.ts
+++ apis/apps/payment/src/payment.service.ts
@@ -1,5 +1,5 @@
-import { ConfigService } from '@nestjs/config';
 import { Body, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { Stripe } from 'stripe';
 
 @Injectable()
