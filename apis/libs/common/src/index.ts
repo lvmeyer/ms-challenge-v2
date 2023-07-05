5b8b1788170b125c7b146790cce2b1237d088989
DanLevyM <dan.levy.pro@gmail.com>
2023-07-05 14:05:28 +0200

    [APIS] reviews

--- apis/libs/common/src/index.ts
+++ apis/libs/common/src/index.ts
@@ -7,6 +7,7 @@ export * from './typeorm/typeorm.module';
 // DTO
 export * from './dtos/products/products.request';
 export * from './dtos/products/categories.request';
+export * from './dtos/products/reviews.request';
 export * from './dtos/basket/basket.request';
 export * from './dtos/auth/auth.request';
 export * from './dtos/users/users.request';
@@ -21,3 +22,4 @@ export * from './errors/ErrorResponse';
 export { Basket } from './entity/Basket';
 export { Product } from './entity/Product';
 export { Category } from './entity/Category';
+export { Review } from './entity/Review';
