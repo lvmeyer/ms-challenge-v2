ec6a514d0482fc700788af14d6ceb36c7bd260cd
pboitelle <pi.boitelle@gmail.com>
2023-07-05 23:52:17 +0200

    pull dev fix no filtre products

--- vite-front/src/pages/Cart/components/CartListing/CartListing.jsx
+++ vite-front/src/pages/Cart/components/CartListing/CartListing.jsx
@@ -1,101 +1,116 @@
-import React, { useEffect, useState } from "react";
-import "./CartListing.css";
+import React, { useEffect, useState } from 'react';
+import './CartListing.css';
 import imgproduct from '../../../../../public/assets/images/nvidia.jpeg';
-import { Link } from "react-router-dom";
-
-
+import { Link } from 'react-router-dom';
 
 export const CartListing = () => {
-const [baskedId, setBaskedId] = useState(0);
-const [userProducts, setUserProducts] = useState([]);
-const [totalAmount, setTotalAmount] = useState(0);
+	const [baskedId, setBaskedId] = useState(0);
+	const [userProducts, setUserProducts] = useState([]);
+	const [totalAmount, setTotalAmount] = useState(0);
 
-useEffect(() => {
-  fetch(import.meta.env.VITE_GW_HOSTNAME+'/api/v1/user-basket', {
-    mode: 'cors',
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer ' + JSON.parse(localStorage.getItem('userInfo')).access_token
-    }
-  })
-  .then(response => response.json())
-  .then(data => {
-    setBaskedId(data.basketId);
+	useEffect(() => {
+		fetch(import.meta.env.VITE_GW_HOSTNAME + '/api/v1/user-basket', {
+			mode: 'cors',
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization:
+					'Bearer ' + JSON.parse(localStorage.getItem('userInfo')).access_token,
+			},
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				setBaskedId(data.basketId);
 
-    fetch(import.meta.env.VITE_GW_HOSTNAME+`/api/v1/basket/${data.basketId}/products`, {
-      mode: 'cors',
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + JSON.parse(localStorage.getItem('userInfo')).access_token
-      }
-    })
-    .then(response => response.json())
-    .then(data => {
-      setUserProducts(data.data.products);
-      setTotalAmount(data.data.price);
-    });
-  });
-}, []);
+				fetch(
+					import.meta.env.VITE_GW_HOSTNAME +
+						`/api/v1/basket/${data.basketId}/products`,
+					{
+						mode: 'cors',
+						method: 'GET',
+						headers: {
+							'Content-Type': 'application/json',
+							Authorization:
+								'Bearer ' +
+								JSON.parse(localStorage.getItem('userInfo')).access_token,
+						},
+					}
+				)
+					.then((response) => response.json())
+					.then((data) => {
+						setUserProducts(data.data.products);
+						setTotalAmount(data.data.price);
+					});
+			});
+	}, []);
 
-  const handleRemoveProductBasket = (productId) => {
-    fetch(import.meta.env.VITE_GW_HOSTNAME+`/api/v1/basket/remove`, {
-      mode: 'cors',
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + JSON.parse(localStorage.getItem('userInfo')).access_token
-      },
-      body: JSON.stringify({
-        basketId: baskedId,
-        productId: productId
-      })
-    })
-    .then(response => response.json())
-    .then(data => {
-      window.location.reload();
-    });
-  }
+	const handleRemoveProductBasket = (productId) => {
+		fetch(import.meta.env.VITE_GW_HOSTNAME + `/api/v1/basket/remove`, {
+			mode: 'cors',
+			method: 'PATCH',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization:
+					'Bearer ' + JSON.parse(localStorage.getItem('userInfo')).access_token,
+			},
+			body: JSON.stringify({
+				basketId: baskedId,
+				productId: productId,
+			}),
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				window.location.reload();
+			});
+	};
 
-  return (
-    <div className="cart-products-container">
-      {userProducts.length > 0 ? userProducts.map((userProduct) => (
-        <div className="cart-product-card" key={userProduct.id}>
-          <div>
-            <img className="cart-img" alt={imgproduct} src={userProduct.image} />
-          </div>
-          <div className="product-description">
-            <h3>{userProduct.name}</h3>
-            <div className="product-description-items">
-              <div>
-                <p><strong>Category :</strong> {userProduct.category.name}</p>
-                <p><strong>Price :</strong> ${userProduct.price}</p>
-              </div>
-              <button className="btn btn-danger text-light"
-              onClick={() => handleRemoveProductBasket(userProduct.id)}>
-                Remove
-              </button>
-            </div>
-          </div>
-        </div>
-      ))
-      : <div id="cart-empty" className="alert alert-info">Your cart is empty</div>
-    }
-    <div className="total-amount">
-      <strong>Total to pay : </strong>
-      ${totalAmount}
-    </div>
-    
-    {userProducts.length > 0 ? 
-      <Link to={`/payment/${baskedId}`}>
-        <button className="pay-button">
-          Pay
-        </button>
-      </Link>
-    : null
-    }
-    
-    </div>
-  );
+	return (
+		<div className="cart-products-container">
+			{userProducts.length > 0 ? (
+				userProducts.map((userProduct) => (
+					<div className="cart-product-card" key={userProduct.id}>
+						<div>
+							<img
+								className="cart-img"
+								alt={imgproduct}
+								src={userProduct.image}
+							/>
+						</div>
+						<div className="product-description">
+							<h3>{userProduct.name}</h3>
+							<div className="product-description-items">
+								<div>
+									<p>
+										<strong>Category :</strong> {userProduct.category.name}
+									</p>
+									<p>
+										<strong>Price :</strong> ${userProduct.price}
+									</p>
+								</div>
+								<button
+									className="btn btn-danger text-light"
+									onClick={() => handleRemoveProductBasket(userProduct.id)}
+								>
+									Remove
+								</button>
+							</div>
+						</div>
+					</div>
+				))
+			) : (
+				<div id="cart-empty" className="alert alert-info">
+					Your cart is empty
+				</div>
+			)}
+			<div className="total-amount">
+				<strong>Total to pay : </strong>${totalAmount}
+			</div>
+
+			{userProducts.length > 0 ? (
+				<Link to={`/payment/${baskedId}`}>
+					<button className="pay-button">Pay</button>
+				</Link>
+			) : null}
+		</div>
+	);
 };
--- vite-front/src/pages/UserProfile/UserProfile.jsx
+++ vite-front/src/pages/UserProfile/UserProfile.jsx
@@ -1,20 +1,35 @@
 import React, { useContext } from "react";
-import { Outlet } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { useState } from "react";
+import { useSelector } from "react-redux";
 
 import "./UserProfile.css";
+import { Profile } from "./Profile/Profile";
+import { AdminCommentPanel } from "./PanelAdmin/AdminCommentPanel";
 
 export const UserProfile = () => {
+  const { userInfo } = useSelector((state) => state.auth);
+
 
   return (
     (
       <div>
         <div className="user-profile-container">
           <div className="link-container">
-            
+
+            {userInfo && (userInfo.role === 'ADMINISTRATOR' ) ? (
+              <>
+            <Profile />
+            <AdminCommentPanel />
+              </>
+            ) : (
+              <>
+              <Profile />
+              
+              </>
+            )}
+
           </div>
-          <Outlet />
         </div>
       </div>
     )
