97ef93c54bf283e899707bf5c9826d395f262c61
victor <valee.victor@gmail.com>
2023-07-06 12:07:12 +0200

    Add Admin product panel

---
+++ vite-front/src/pages/UserProfile/Profile/Product/EditProduct.jsx
@@ -0,0 +1,110 @@
+import React, { useState, useEffect } from "react";
+import "../Profile.css";
+
+
+export const EditProduct = () => {
+
+
+//     // const handleCancelClick = () => {
+//     //     // setEditMode(false);
+//     //     // setEditProductId(null);
+//     // };
+
+
+//     // const [editProductData, setEditProductData] = useState({});
+//     // const [product, setProduct] = useState({});
+//     // const [editProductId, setEditProductId] = useState(null);
+
+
+//     // const handleUpdate = () => {
+//     //     console.log(editProductId);
+    
+//     //       fetch(import.meta.env.VITE_GW_HOSTNAME+'/api/v1/products/' + editProductId, {
+//     //         method: 'PATCH',
+//     //         headers: {
+//     //           'Content-Type': 'application/json',
+//     //           'Authorization': 'Bearer ' + JSON.parse(localStorage.getItem('userInfo')).access_token
+//     //         },
+//     //         body: JSON.stringify(editProductData)
+//     //       })
+//     //         .then(response => response.json())
+//     //         .then(data => {
+//     //           console.log(data);
+//     //           setProduct(data);
+//     //         //   setEditProductId(null);
+//     //         })
+//     //         .catch(error => {
+//     //           console.log(error);
+//     //         });
+//     //   };
+
+//     // const handleSubmit = (e) => {
+//     //     e.preventDefault();
+//     //     console.log(editProductData);
+
+//     //     handleUpdate();
+//     // }
+    
+
+
+    return (
+
+        <div>azertyu</div>
+
+//     <form 
+//     // onSubmit={handleSubmit} 
+//     className="d-flex flex-column">
+//         <input
+//         className="form-control"
+//         name="name"
+//         placeholder="Name"
+//         // value={editProductData.name}
+//         // onChange={handleChange}
+//         id="name"
+//         />
+//         <input
+//         className="form-control"
+//         name="price"
+//         placeholder="Price"
+//         // value={editProductData.price}
+//         // onChange={handleChange}
+//         id="price"
+//         />
+//         <input
+//         className="form-control col"
+//         name="description"
+//         placeholder="Description"
+//         // value={editProductData.description}
+//         // onChange={handleChange}
+//         id="description"
+//         />
+//         <input
+//         className="form-control"
+//         name="image"
+//         placeholder="Image"
+//         // value={editProductData.image}
+//         // onChange={handleChange}
+//         id="image"
+//         />
+//         <select
+//         name="category"
+//         // value={editProductData.category}
+//         // onChange={handleChange}
+//         className="form-select form-select-sm"
+//         aria-label=".form-select-sm example"
+//         >
+//         {/* <option value="">Select a category</option>
+//         {categories.map((category) => (
+//             <option key={category.id} value={category.id}>
+//             {category.name}
+//             </option>
+//         ))} */}
+//         </select>
+
+//         <button type="submit" className="btn btn-primary w-25">Save</button>
+//         {/* <button type="button" className="btn btn-secondary w-25" onClick={handleCancelClick}>Cancel</button> */}
+//     </form>
+    );
+
+
+}
